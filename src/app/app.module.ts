a733a3fc9562f33a1914cec3ec32a20e10bf655c
bfischb1 <bfischb1@gmail.com>
2022-07-12 13:02:44 -0400

    5 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,18 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
+    DashboardComponent,
   ],
   bootstrap: [AppComponent],
 })
